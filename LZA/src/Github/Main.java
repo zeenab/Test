39aec2cd7542f72ea65fa856987a8a452ab743af
Zeenab Fathi Mohammad Ramlawi <201706591@NATIVITY.BETHLEHEM.EDU>
2018-05-02 10:22:16 +0300

    add new method

--- LZA/src/Github/Main.java
+++ LZA/src/Github/Main.java
@@ -1,10 +1,21 @@
 package Github;
-
+import java.util.ArrayList;
+import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+		
+	Scanner input =new Scanner(System.in);
 	Course c = new Course("SWER142", "Object Oriented Prog", null);
-
+	ArrayList <Double> arr = new ArrayList<>();
+	System.out.println(" enter a number");
+	
+	double  i;
+do {
+	
+	i =input.nextInt();
+	arr.add(i);
+} while ( i != 0);
 	}
 
 }
