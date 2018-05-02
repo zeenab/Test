7f27cbfd807b94991fb8a902313e55c0140f800b
Aziza Ishaq Saeed Sayed Ahmad <201706418@NATIVITY.BETHLEHEM.EDU>
2018-05-02 10:37:58 +0300

    lllaaazzz

--- LZA/src/Github/Main.java
+++ LZA/src/Github/Main.java
@@ -1,21 +1,39 @@
 package Github;
+
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
-	Scanner input =new Scanner(System.in);
-	Course c = new Course("SWER142", "Object Oriented Prog", null);
-	ArrayList <Double> arr = new ArrayList<>();
-	System.out.println(" enter a number");
-	
-	double  i;
-do {
-	
-	i =input.nextInt();
-	arr.add(i);
-} while ( i != 0);
+
+		Scanner input = new Scanner(System.in);
+		Course c = new Course("SWER142", "Object Oriented Prog", null);
+		ArrayList<Double> arr = new ArrayList<>();
+		System.out.println(" enter a number");
+
+		double i;
+		do {
+
+			i = input.nextInt();
+			arr.add(i);
+		} while (i != 0);
+	}
+
+	public static double max(ArrayList<Double> list) {
+		if (list.size() == 0)
+			return (Double) null;
+		Double max = list.get(0);
+
+		for (int i = 0; i < list.size(); i++) {
+
+			if (list.get(i) > max) {
+				max = list.get(i);
+			}
+		}
+		return max;
+
 	}
 
 }
