2ac92763f3a8b2966bd8bc00da0abbbc20192d0b
Alex Suhail Jabra Dukmak <201706407@NATIVITY.BETHLEHEM.EDU>
2018-05-02 10:05:54 +0300

    update main

--- LZA/src/Github/Main.java
+++ LZA/src/Github/Main.java
@@ -3,7 +3,7 @@ package Github;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	Course c = new Course("SWER142", "Object Oriented Prog", null);
 
 	}
 
